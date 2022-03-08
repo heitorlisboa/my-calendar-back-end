74ccbeeee83b865e6046f9e00c7b76d188ddfeba
Heitor Lisboa <89428328+heitorlisboa@users.noreply.github.com>
2022-03-08 01:02:39 -0300

    Alteração na `TaskDayView`
    * Agora a view exibe corretamente o id das `tasks` filhas

--- back-end/my_calendar/lib/my_calendar_web/views/task_day_view.ex
+++ back-end/my_calendar/lib/my_calendar_web/views/task_day_view.ex
@@ -1,6 +1,8 @@
 defmodule MyCalendarWeb.TaskDayView do
   use MyCalendarWeb, :view
 
+  alias MyCalendarWeb.TaskView
+
   def render("index.json", %{task_days: task_days}) do
     render_many(task_days, __MODULE__, "task_day.json")
   end
@@ -9,7 +11,7 @@ defmodule MyCalendarWeb.TaskDayView do
     %{
       id: task_day.id,
       date: task_day.date,
-      tasks: task_day.tasks
+      tasks: render_many(task_day.tasks, TaskView, "task.json")
     }
   end
 end
