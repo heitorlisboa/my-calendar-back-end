42002ccc354ab2bbff9d34f209b48008e93ca9da
heitorlisboa <heitor01101000@gmail.com>
2022-03-13 14:02:05 -0300

    Ajustes no fallback controller

--- lib/my_calendar_web/controllers/fallback_controller.ex
+++ lib/my_calendar_web/controllers/fallback_controller.ex
@@ -2,14 +2,28 @@ defmodule MyCalendarWeb.FallbackController do
   use MyCalendarWeb, :controller
 
   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    error_messages =
+      changeset.errors
+      |> Enum.map(fn error ->
+        {_field, {message, _cause}} = error
+        message
+      end)
+
     conn
     |> put_status(400)
-    |> json(%{error: "Changeset error", changeset: changeset})
+    |> json(%{error: error_messages})
   end
 
   def call(conn, {:error, status}) when is_atom(status) do
+    status_code =
+      try do
+        Plug.Conn.Status.code(status)
+      rescue
+        _invalid_status_code_error -> :unauthorized
+      end
+
     conn
-    |> put_status(status)
+    |> put_status(status_code)
     |> json(%{error: status})
   end
 end
